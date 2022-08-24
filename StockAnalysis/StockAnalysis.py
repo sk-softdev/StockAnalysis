a090c7c18f5690630638d1a257b18198c9c6dc4f
sck-dev <waterdrinker76@gmail.com>
2022-08-24 13:44:35 -0500

    Many improvements
    Fixed concurrency so multiple users can use the site at once
    Fixed deletion to more accurately update heights of tables
    Reduced redundancy by only updating market data and individual user data when they first access the site for the day
    Fixed other misc. errors with data updates
    Optimized code to be clean and readable

--- StockAnalysis/StockAnalysis.py
+++ StockAnalysis/StockAnalysis.py
@@ -6,17 +6,11 @@ import requests, time, json, random, sys
 
 bp = Blueprint("StockAnalysis", __name__)
 
-# This function takes in ticker to be added from html form to relay relevant data (appended to arr) to website
-# The g name stands for “global”, but that is referring to the data being global within a context.
-# The data on g is lost after the context ends, and it is not an appropriate place to store data between requests.
-# Use the session or a database to store data across requests.
-
-# bug = one user running and another then when i add a stock it erases everything else except the new stock?
+# StockAnalysis, otherwise known as Stockie, is a stock analysis webapp that allows user to track stocks as well as research them.
 
 @bp.route("/", methods=['GET', 'POST'])
 def home():
-    currentYear = str(date.today().year)
-    holidays = [currentYear + "-01-17", currentYear + "-02-21", currentYear + "-04-15", currentYear + "-05-30", currentYear + "-06-20", currentYear + "-07-04", currentYear + "-09-05", currentYear + "-11-24", currentYear + "-12-26"]
+    # initial data set-up for session
     if 'dataCreated' not in session:
         session['dataCreated'] = False
         session['userDataCreated'] = False
@@ -25,13 +19,8 @@ def home():
         session['userFilter'] = ""
         session['noStocksAfterCreation'] = True
 
-    dataCreated = session.get("dataCreated")
-    userDataCreated = session.get("userDataCreated")
-    tableHeight = session.get("tableHeight")
-    newsHeight = session.get("newsHeight")
-    userFilter = session.get("userFilter")
-    noStocksAfterCreation = session.get("noStocksAfterCreation")
-
+    # determines the day and adjusts to be a weekday
+    # polygon api doesn't allow data of the current day to be accessed for free (assuming it's a business day), so the day must be set back by one
     if datetime.today().weekday() - 1 < 0:
         today = date.today() - timedelta(days = 3)
     elif datetime.today().weekday() - 1 < 5:
@@ -39,11 +28,15 @@ def home():
     else:
         today = date.today() - timedelta(days = datetime.today().weekday() - 4)
 
+    currentYear = str(date.today().year)
+    holidays = [currentYear + "-01-17", currentYear + "-02-21", currentYear + "-04-15", currentYear + "-05-30", currentYear + "-06-20", currentYear + "-07-04", currentYear + "-09-05", currentYear + "-11-24", currentYear + "-12-26"]
+    
+    # ensures the day isn't a holiday as the market wouldn't of been open
     if str(today) in holidays:
         today = today - timedelta(days = 1)
 
     if request.method == 'POST':
-        noStocksAfterCreation = False
+        session['noStocksAfterCreation'] = False
         duplicate = False
         stored = False
         ticker = request.form.get('tckr')
@@ -56,67 +49,85 @@ def home():
                     duplicate = True
                     break
 
+        # checks to see if input is a duplicate stock. if not, then the stock is added
         if (duplicate == False):
             try:
                 addStock(ticker, today, stored)
             except:
                 flash("Oops!" + str(sys.exc_info()[0]) + "occurred.")
                 flash("Please wait 5 minutes then refresh. The API calls are limited.")
-            tableHeight = session.get("tableHeight")
-            newsHeight = session.get("newsHeight")
         else:
             flash("You're already tracking " + ticker + ".", "info")
 
-    # initial data creation if stored data exists, shift + tab to go left
-    if g.user is not None and userDataCreated == False:
-        if g.user['stocks'] is not None:
-            noStocksAfterCreation = False
+    # adds data previously stored in the user database
+    if g.user != None and session['userDataCreated'] == False:
+        if g.user['stocks'] != None and today != g.user['lastLogin']:
+            session['noStocksAfterCreation'] = False
             session['userDataCreated'] = True
-            userDataCreated = True
+
             tickers = g.user['stocks']
             tickersList = tickers.split(",")
+
             db = get_db()
-            db.execute("DELETE FROM userStats")
+
+            # clears out previous data to update
+            db.execute("DELETE FROM userStats WHERE username = ?", (g.user['username'],))
             db.commit()
+
             stored = True
+
+            # add all stocks retrieved from database
             for ticker in tickersList:
                 try:
                     addStock(ticker, today, stored)
                 except:
-                    noStocksAfterCreation = True
-                    userDataCreated = False
                     session['userDataCreated'] = False
                     session['noStocksAfterCreation'] = True
                     flash("Oops!" + str(sys.exc_info()[0]) + "occurred.")
                     flash("Please wait 5 minutes then refresh. The API calls are limited.")
                     break
-                tableHeight = session.get("tableHeight")
-                newsHeight = session.get("newsHeight")
+        else:
+            session['userDataCreated'] = True
+            if g.user['stocks'] != None:
+                session['noStocksAfterCreation'] = False
+                session['tableHeight'] = g.user['stocks'].split(",").length * 75
+                session['newsHeight'] = g.user['stocks'].split(",").length * 75
 
-    if dataCreated == False:
+        # logs login date to avoid excessive data updates
+        db = get_db()
+        db.execute("UPDATE user SET lastLogin = ? WHERE id = ?", (today, g.user['id']))
+        db.commit()
+
+    # initial market data creation
+    if session['dataCreated'] == False:
         try:
-            dataCreate(today)
-            dataCreated = True
+            db = get_db()
+            # checks if market data has already been created
+            dateUpdated = db.execute("SELECT dateUpdated FROM marketStats").fetchone()           
+            if dateUpdated != today:
+                dataCreate(today)
+
             session['dataCreated'] = True
         except:
             flash("Oops!" + str(sys.exc_info()[0]) + "occurred.")
             flash("Please wait 5 minutes then refresh. The API calls are limited.")
 
+    # stores market data
     db = get_db()
-    marketWinners = db.execute("SELECT * FROM marketStats " + userFilter + " ORDER BY change DESC LIMIT 10").fetchall()
-    marketLosers = db.execute("SELECT * FROM marketStats " + userFilter + " ORDER BY change LIMIT 10").fetchall()
-    marketPopular = db.execute("SELECT * FROM marketStats " + userFilter + " ORDER BY volume DESC LIMIT 10").fetchall()
-    if g.user is not None:
+    marketWinners = db.execute("SELECT * FROM marketStats " + session.get("userFilter") + " ORDER BY change DESC LIMIT 10").fetchall()
+    marketLosers = db.execute("SELECT * FROM marketStats " + session.get("userFilter") + " ORDER BY change LIMIT 10").fetchall()
+    marketPopular = db.execute("SELECT * FROM marketStats " + session.get("userFilter") + " ORDER BY volume DESC LIMIT 10").fetchall()
+
+    # retrieves user data, if a user is logged in 
+    if g.user != None:
         userStats = db.execute("SELECT * FROM userStats WHERE username = ?", (g.user['username'],)).fetchall()
     else:
-        userStats = None    
-    return render_template("index.html", dataCreated=dataCreated, userDataCreated=userDataCreated, tableHeight=tableHeight, newsHeight=newsHeight, marketWinners=marketWinners, marketLosers=marketLosers, marketPopular=marketPopular, userStats=userStats, noStocksAfterCreation=noStocksAfterCreation, today=today)
+        userStats = None
 
-def addStock(ticker, today, stored):
-    session['noStocksAfterCreation'] = False
-    key = 'XoIHGXzAz89flV3zUjNGpOEf9zJ0iidW'
-    tableHeight = session.get("tableHeight")
-    newsHeight = session.get("newsHeight")
+    return render_template("index.html", dataCreated=session.get("dataCreated"), userDataCreated=session.get("userDataCreated"), tableHeight=session.get("tableHeight"), newsHeight=session.get("newsHeight"), marketWinners=marketWinners, marketLosers=marketLosers, marketPopular=marketPopular, userStats=userStats, noStocksAfterCreation=session.get("noStocksAfterCreation"), today=today)
+
+def setStockData(ticker):
+    key = 'placeholder'
     req = requests.get("https://api.polygon.io/v1/meta/symbols/" + ticker + "/company?apiKey=" + key)
     data = json.loads(req.content)
     name = data['name']
@@ -133,40 +144,53 @@ def addStock(ticker, today, stored):
     newsTitle = data['results'][1]['title']
     newsPublisher = data['results'][0]['publisher']['name']
     newsDate = data['results'][3]['published_utc']
+    
+    # cuts news date to just include the date
+    for x in newsDate:
+        if x == "T":
+            newsDate = newsDate[:newsDate.index(x)]
+            break
+
     newsURL = data['results'][4]['article_url']
-    session['tableHeight'] = tableHeight + 75
-    session['newsHeight'] = newsHeight + 75
+
+    stockData = [name, ticker, logo, tickerID, buttonID, stockPrice, priceChange, volume, newsTitle, newsPublisher, newsDate, newsURL]
+
+    return stockData
+
+def addStock(ticker, today, stored):
+    session['noStocksAfterCreation'] = False
+    session['tableHeight'] = session.get("tableHeight") + 75
+    session['newsHeight'] = session.get("newsHeight") + 75
+    
+    stockData = setStockData(ticker)
+
     db = get_db()
     db.execute(
         "INSERT INTO userStats (username, name, ticker, logo, tickerID, buttonID, stockPrice, priceChange, volume, newsTitle, newsPublisher, newsDate, newsURL) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-        (g.user['username'], name, ticker, logo, tickerID, buttonID, stockPrice, priceChange, volume, newsTitle, newsPublisher, newsDate, newsURL),
+        (g.user['username'], stockData[0], stockData[1], stockData[2], stockData[3], stockData[4], stockData[5], stockData[6], stockData[7], stockData[8], stockData[9], stockData[10], stockData[11]),
     )
-    db.commit()
 
+    # updates user's stocks in db, if not retrieving previously stored stocks
     if stored == False:
-        user_id = session.get("user_id")
         db = get_db()
-        if (g.user['stocks'] is None): # initial
+        if (g.user['stocks'] == None): # initial
             db.execute("UPDATE user SET stocks = ? WHERE id = ?", (ticker, g.user['id']))
         else: # update after initial creation
             db.execute("UPDATE user SET stocks = ? WHERE id = ?", (g.user['stocks'] + "," + ticker, g.user['id']))
-        db.commit()
+    
+    db.commit()
 
     return
-
 
 def dataCreate(today):
     db = get_db()
     db.execute("DELETE FROM marketStats")
-    db.commit()
-    key = 'XoIHGXzAz89flV3zUjNGpOEf9zJ0iidW'
+    db.execute("INSERT INTO marketStats (dateUpdated, change) VALUES (?, ?)", (today, 0))
+    key = 'placeholder'
     req = requests.get("https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/" + str(today) + "?adjusted=true&apiKey=" + key)
     data = json.loads(req.content)
-    # defines biggest losers and biggest winners across the entire stock market idea: do biggest gainers and losers by market cap and shares traded on the day
-    # 8 categories: All, less than 100 mil, less than 50 mil, less than 25 mil, less than 10 mil, less than 5 mil, less than 1 mil, less than 500k
-    # Do analysis across a week of the volumes??
     for x in data['results']:
-        if "." not in x['T']:
+        if len(x['T']) < 5: # excludes warrants/issues etc
             db.execute(
                 "INSERT INTO marketStats (ticker, volume, change) VALUES (?, ?, ?)",
                 (x['T'], x['v'], round(((x['c'] - x['o']) / x['o']) * 100, 2)),
@@ -207,76 +231,73 @@ def filter(sharesTraded):
 
     return redirect("/")
 
-# This function takes in string from url (ex: /delete/AAPL,TSLA,NVDA) then splits that into a list and deletes the matching array indices in data (dataArr)
+# This function takes in string from url (ex: /delete/AAPL,TSLA,NVDA) then splits that into a list and deletes the matching array indices
 @bp.route("/delete/<string:tickers>")
 def delete(tickers):
-    tableHeight = session.get("tableHeight")
-    newsHeight = session.get("newsHeight")
-    tickersList = tickers.split(",")
-
-    # Nested loop sorts through tickers submitted via tickersList and deletes them if they match previously stored data (dataArr)
-    # Iterates backwards to avoid out of index errors when removing
 
     db = get_db()
+    tickersList = tickers.split(",")
+
+    # Nested loop sorts through tickers submitted via tickersList and deletes them from user data.
+    # Iterates backwards to avoid out of index errors when removing.
 
     for y in tickersList:
-        session['tableHeight'] = tableHeight - 75
-        session['newsHeight'] = newsHeight - 75
+        session['tableHeight'] = session['tableHeight'] - 75
+        session['newsHeight'] = session['newsHeight'] - 75
         db.execute("DELETE FROM userStats WHERE ticker = ?", (y,))
         db.commit()
 
-    userTickers = g.user['stocks'] # stores all users stocks from database then splits them into list below
+    userTickers = g.user['stocks']
     userTickers = userTickers.split(",")
 
+    # removes deleted stock from user's stocks
     for i in range(len(userTickers) - 1, -1, -1):
         for j in range(len(tickersList)):
             if userTickers[i] == tickersList[j]:
                 del userTickers[i]
-                break # break to avoid out of index error for the rest of j's after i is removed
-
-    dataString = ""
+                break
+    
+    # recreates user's stocks after stock has been removed
+    stocksString = ""
     for x in userTickers:
         if x == userTickers[0]:
-            dataString = x
+            stocksString = x
         else:
-            dataString = dataString + "," + x
+            stocksString = stocksString + "," + x
 
-    if dataString == "":
+    if stocksString == "":
         session['noStocksAfterCreation'] = True
+        stocksString = None
 
     db = get_db()
-    db.execute("UPDATE user SET stocks = ? WHERE id = ?", (dataString, g.user['id']))
+    db.execute("UPDATE user SET stocks = ? WHERE id = ?", (stocksString, g.user['id']))
     db.commit()
 
     return redirect("/")
 
-# Iterates through financials of desired company to research and
+# Iterates through financials of desired company to research and stores them 
 @bp.route("/research", methods=['GET', 'POST'])
 def research():
     if request.method == 'POST':
         ticker = request.form.get('tckr')
-        req = requests.get("https://api.polygon.io/vX/reference/financials?ticker=" + ticker + "&apiKey=XoIHGXzAz89flV3zUjNGpOEf9zJ0iidW")
-        data = json.loads(req.content)
-        db = get_db()
-        db.execute("DELETE FROM researchStats")
-        db.commit()
+        researchArr = []
         try:
-            db = get_db()
+            req = requests.get("https://api.polygon.io/vX/reference/financials?ticker=" + ticker + "&apiKey=placeholder")
+            data = json.loads(req.content)
             researchData = [[data['results'][0]['financials']['comprehensive_income'], "Comprehensive Income"], [data['results'][0]['financials']['income_statement'], "Income Statement"], [data['results'][0]['financials']['balance_sheet'], "Balance Sheet"], [data['results'][0]['financials']['cash_flow_statement'], "Cash Flow Statement"]]
+
             for x in researchData:
+                # stores section title stored in second index. ex: "Comprehensive Income"
                 section = x[1]
+                
+                # iterates through data stored in first index
                 for y in x[0]:
-                    db.execute(
-                        "INSERT INTO researchStats (label, value, section) VALUES (?, ?, ?)",
-                        (x[0][y]['label'], x[0][y]['value'], section),
-                    )
+                    researchArr.append([x[0][y]['label'], x[0][y]['value'], section])
                     section = ""
-            db.commit()
         except:
             flash("Oops!" + str(sys.exc_info()[0]) + "occurred.")
             flash("Please wait 5 minutes then refresh. The API calls are limited.")
-
-        researchStats = db.execute("SELECT * FROM researchStats").fetchall()
-        return render_template("research.html", researchStats=researchStats, ticker=ticker)
+        
+        return render_template("research.html", researchArr=researchArr, ticker=ticker)
 
     return render_template("research.html")
--- StockAnalysis/schema.sql
+++ StockAnalysis/schema.sql
@@ -1,19 +1,20 @@
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS userStats;
 DROP TABLE IF EXISTS marketStats;
-DROP TABLE IF EXISTS researchStats;
 
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   username TEXT UNIQUE NOT NULL,
   password TEXT NOT NULL,
-  stocks TEXT
+  stocks TEXT,
+  lastLogin TEXT
 );
 
 CREATE TABLE marketStats (
-  ticker TEXT NOT NULL,
-  volume INTEGER NOT NULL,
-  change FLOAT NOT NULL
+  ticker TEXT,
+  volume INTEGER,
+  change FLOAT,
+  dateUpdated TEXT
 );
 
 CREATE TABLE userStats (
@@ -30,10 +31,4 @@ CREATE TABLE userStats (
   newsPublisher TEXT NOT NULL,
   newsDate TEXT NOT NULL,
   newsURL TEXT NOT NULL
-);
-
-CREATE TABLE researchStats (
-  label TEXT NOT NULL,
-  value FLOAT NOT NULL,
-  section TEXT NOT NULL
 );
